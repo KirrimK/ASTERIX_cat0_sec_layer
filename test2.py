b86a9ceec357bf5f66094596a20ef0683d2d74d8
KirrimK <KirrimK@users.noreply.github.com>
2022-06-09 13:35:31 +0200

    added checking max time to test 2

--- test2.py
+++ test2.py
@@ -19,25 +19,35 @@ def sign(message, pkey):
 if __name__ == '__main__':
     private_key = SigningKey.generate()
     public_key = private_key.verify_key
-    lst_message = radar(10000)
+    number = 10000
+    lst_message = radar(number)
     lst_signed = []
-    tic = time.process_time()
+    global_start_time = time.process_time()
+    max_sign_time = 0
+    max_verify_time = 0
     for m in lst_message:
+        local_sign_start = time.process_time()
         signed = sign(m, private_key)
+        max_sign_time = max(max_sign_time, time.process_time() - local_sign_start)
         lst_signed.append(signed)
-    tac = time.process_time()
+    global_all_signed_time = time.process_time()
     for i,s in enumerate(lst_signed):
         try:
+            local_verify_start = time.process_time()
             public_key.verify(lst_message[i], s)
+            max_verify_time = max(max_verify_time, time.process_time() - local_verify_start)
         except BadSignatureError:
             print('Wrong signature')
-    toc = time.process_time()
-    print('Signing time: {} ms\n'.format((tac-tic)*1000))
-    print('Decoding time: {} ms\n'.format( (toc - tac) * 1000) )
-    print('Total time: {} ms\n'.format( (toc - tic) * 1000) )
-    print('Signing average time: {} ms\n'.format( ((tac - tic) / 10000)* 1000) )
-    print('Decoding average time: {} ms\n'.format( ((toc - tac) / 10000) * 1000) )
-    print('Both average time: {} ms\n'.format( ((toc - tic) / 10000) * 1000) )
+    global_all_verified_time = time.process_time()
+    print('Signing time: {} ms'.format((global_all_signed_time-global_start_time)*1000))
+    print('Decoding time: {} ms'.format( (global_all_verified_time - global_all_signed_time) * 1000) )
+    print('Total time: {} ms\n'.format( (global_all_verified_time - global_start_time) * 1000) )
+    print('Signing average time: {} ms'.format( ((global_all_signed_time - global_start_time) / number)* 1000) )
+    print('Decoding average time: {} ms'.format( ((global_all_verified_time - global_all_signed_time) / number) * 1000) )
+    print('Both average time: {} ms\n'.format( ((global_all_verified_time - global_start_time) / number) * 1000) )
+    print('Max signing time: {} ms'.format( max_sign_time * 1000))
+    print('Max verify time: {} ms'.format( max_verify_time * 1000))
+
 
 
 
