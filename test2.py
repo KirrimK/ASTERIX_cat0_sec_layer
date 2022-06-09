71062eaaf6d3a8f3f47eabe5fdd1f41d9e7b201b
Humbert Andy <humbert.andy@alumni.enac.fr>
2022-06-09 15:40:16 +0200

    from list to file

--- test2.py
+++ test2.py
@@ -1,33 +1,53 @@
 import base64
 from nacl.signing import SigningKey
 from nacl.exceptions import BadSignatureError
+from nacl.encoding import Base64Encoder
 import random
 import time
 
+ORIG_FILE_LCT = 'tests/radar.txt'
+
 def radar(n):
-    res = []
-    for _ in range(n):
-        res.append(bytes((''.join(str(random.randint(0, 1)) for _ in range(48))), encoding="utf-8"))
-    return res
+    with open(ORIG_FILE_LCT, 'wb+') as rad:
+        for _ in range(n):
+            temp = random.randbytes(48)
+            rad.write(base64.b64encode(temp))
+            rad.write(b'\n')
+
 
 def sign(message, pkey):
-    return pkey.sign(message).signature
+    return pkey.sign(message)
 
 if __name__ == '__main__':
     private_key = SigningKey.generate()
+    with open('tests/privateKey.txt', 'wb+') as prKey:
+        prKey.write(private_key.encode(encoder=Base64Encoder))
     public_key = private_key.verify_key
+    with open('tests/publicKey.txt', 'wb+') as puKey:
+        puKey.write(public_key.encode(encoder=Base64Encoder))
     lst_message = radar(10000)
-    lst_signed = []
     tic = time.process_time()
-    for m in lst_message:
-        signed = sign(m, private_key)
-        lst_signed.append(signed)
+
+    with open(ORIG_FILE_LCT, 'rb+') as f:
+        with open('tests/signed.txt', 'wb+') as fic:
+            fic.truncate()
+            for line in f:
+                lineCleanedBytes = line.strip()
+                signed = private_key.sign(lineCleanedBytes, encoder=Base64Encoder)
+                fic.write(signed)
+                fic.write(b'\n')
+
     tac = time.process_time()
-    for i,s in enumerate(lst_signed):
-        try:
-            public_key.verify(lst_message[i], s)
-        except BadSignatureError:
-            print('Wrong signature')
+
+    with open('tests/signed.txt', 'rb+') as f2:
+        for line in f2:
+            lineCleanedb = line.strip()
+
+            try:
+                public_key.verify(lineCleanedb, encoder=Base64Encoder)
+            except BadSignatureError:
+                print('Wrong signature')
+
     toc = time.process_time()
     print('Signing time: {} ms\n'.format((tac-tic)*1000))
     print('Decoding time: {} ms\n'.format( (toc - tac) * 1000) )
@@ -35,7 +55,6 @@ if __name__ == '__main__':
     print('Signing average time: {} ms\n'.format( ((tac - tic) / 10000)* 1000) )
     print('Decoding average time: {} ms\n'.format( ((toc - tac) / 10000) * 1000) )
     print('Both average time: {} ms\n'.format( ((toc - tic) / 10000) * 1000) )
-
 
 
 
