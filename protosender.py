f749bc139893981b455f959955a3b8115d0e5f47
KirrimK <KirrimK@users.noreply.github.com>
2022-06-13 13:55:34 +0200

    send_recv tests

---
+++ protosender.py
@@ -0,0 +1,20 @@
+import main
+import socket
+
+UDP_IP = "192.168.1.172"
+UDP_PORT = 5005
+message = bytearray(48)
+message[:11] = b"Hello World"
+message[45:] = b'fin'
+
+print("message: %s" % message)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+key = bytes(20)
+print("key: {}".format(key))
+big_msg = main.sign_and_assemble_message_sha1_hash(message, key)
+print("big_msg: {}".format(big_msg))
+print("len(big_msg): {}".format(len(big_msg)))
+sock.sendto(big_msg, (UDP_IP, UDP_PORT))
---
+++ receiver.py
@@ -0,0 +1,26 @@
+import main
+import socket
+
+UDP_IP = "192.168.1.172"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+key = bytes(20)
+print("key: {}".format(key))
+key_dict = {main.hash_sha1(key): key}
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    try:
+        print("received message from {}: {}".format(addr, data))
+        print("\\ of len: "+ str(len(data)))
+        ok, decoded = main.disassemble_and_verify_msg_sha1_hash(data, key_dict)
+        print("decoded message: {}".format(decoded))
+        if ok:
+            print("message verified")
+        else:
+            print("message not verified")
+    except Exception as e:
+        print(e)
