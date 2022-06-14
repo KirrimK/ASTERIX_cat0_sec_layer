febcc42dddf709a111085d8397077088f0144658
KirrimK <KirrimK@users.noreply.github.com>
2022-06-14 09:42:41 +0200

    tweaks

--- protosender.py
+++ protosender.py
@@ -3,10 +3,10 @@ import socket
 import random
 import time
 
-UDP_IP = "192.168.1.172"
+UDP_IP = "192.168.1.174"
 UDP_PORT = 42069
 
-N=100000
+N=1000
 
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -21,6 +21,7 @@ while(N>0):
     big_msg = main.sign_and_assemble_message_sha1_hash(message, key)
     #print("big_msg: {}".format(big_msg))
     #print("len(big_msg): {}".format(len(big_msg)))
+    #time.sleep(0.0001)
     sock.sendto(big_msg, (UDP_IP, UDP_PORT))
     N=N-1
 end=time.time()
