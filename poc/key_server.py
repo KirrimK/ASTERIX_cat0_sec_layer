a70eaf1685789158be085c723e2e9f3a12054ae1
KirrimK <KirrimK@users.noreply.github.com>
2022-06-20 15:52:43 +0200

    changed some prints

--- poc/key_server.py
+++ poc/key_server.py
@@ -14,34 +14,34 @@ key_dict: dict[bytes] = {}
 dict_hash = lib.sha1_of_dict(key_dict)
 while True:
     data, (addr, port) = sock.recvfrom(1024)
-    print("Contacted by "+str(addr))
     if len(data) == 32+20+1: #radar add key
+        print("Contacted by server at "+str(addr), end="")
         hash_ = data[32:32+20]
         new_key = lib.VerifyKey(data[:32])
         is_ok = data[20+32]
         comp_hash = lib.key_hash1(new_key)
         if comp_hash != hash_:
-            print("Key integrity problem")
+            print(": Key integrity problem")
         else:
             if is_ok == 1:
                 key_dict[hash_] = new_key._key
-                print("Radar key added")
+                print(": Radar key added")
             else:
                 key_dict.pop(hash_)
-                print("Radar key removed")
+                print(": Radar key removed")
             dict_hash = lib.sha1_of_dict(key_dict)
     elif len(data) == 20: #client asking hash of key
         hash_ = data[:20]
-        print("Received hash from client: {}".format(hash_))
+        print("Received list hash from client at "+str(addr), end="")
         if dict_hash != hash_:
-            print("List has changed since client last fetched, sending update")
+            print(": List is outdated, sending update")
             payload = len(key_dict.keys()).to_bytes(1, 'little')
             for key, value in key_dict.items():
                 payload += key + value
             sock.sendto(payload, (addr, CLIENT_PORT))
         else:
-            print("List is up to date")
+            print(": List is up to date")
             payload = b'\x00'+hash_
             sock.sendto(payload, (addr, CLIENT_PORT))
     else:
-        print("rubbish message: "+str(data))
+        print("Rubbish message from {}: {}".format(addr, str(data)))
