4d576f4a0e8628343a46c1c557712590ac396ff2
HugoCourtadon <hugo.courtadon06@gmail.com>
2022-06-27 14:17:44 +0200

    client_hmac fait

---
+++ poc/client_hmac.py
@@ -0,0 +1,45 @@
+#Client multicast utilisant les clé symétriques
+#Client UDP recevant les messages Asterix et contactant le serveur
+
+
+import socket
+from time import time
+import lib_hmac
+import struct
+
+
+
+#Creation du socket
+CLIENT_IP = "127.0.0.1"
+CLIENT_PORT = 42069
+
+
+
+multicast_group = "224.1.1.1"
+radar_adress = ('',10000)
+
+TIME_BETWEEN_UPDATES = 15 #seconds
+
+sock1= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock1.bind(radar_adress)
+group = socket.inet_aton(multicast_group)
+mreq=struct.pack('4sL',group,socket.INADDR_ANY)
+sock1.setsockopt(socket.IPPROTO_IP,socket.IP_ADD_MEMBERSHIP,mreq)
+
+
+#Clé symétrique
+SECRET = bytes(20)
+
+
+
+
+
+
+#Boucle de process
+while True:
+    #Reception des messages du radar
+    data, addr = sock1.recvfrom(1024)
+    if data :
+        msg,flag=lib_hmac.disassemble_and_verify_msg_sha1(data,SECRET)
+        print(("[VERIFIED] " if flag else "[UNVERIFIED] ")+str(msg))
+
