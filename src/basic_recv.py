3994d4fe1531d32a0b8e61d949cf41f3f3492894
KirrimK --global <KirrimK.users.noreply@github.com>
2022-08-11 14:33:37 +0200

    made basic sender and receiver to test gateway mode

---
+++ src/basic_recv.py
@@ -0,0 +1,21 @@
+"""
+ASTERIX Cat0 Security Layer
+Basic Receiver
+
+The basic receiver plays the role of a legacy system that is not compatible with the security layer without a gateway.
+Its only role is to receive messages over UDP multicast, as a basic ASTERIX endpoint should.
+"""
+
+import socket, struct
+
+MULTICAST_IP = input("multicast ip?")
+MULTICAST_PORT = int(input("multicast port?"))
+
+sockmt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sockmt.bind(('', MULTICAST_PORT))
+groupmt = socket.inet_aton(MULTICAST_IP)
+mreq=struct.pack('4sL',groupmt,socket.INADDR_ANY)
+sockmt.setsockopt(socket.IPPROTO_IP,socket.IP_ADD_MEMBERSHIP,mreq)
+while True:
+    data, (addr, _) = sockmt.recvfrom(1024)
+    print(f"Message from {addr}: "+str(data))
---
+++ src/basic_sender.py
@@ -0,0 +1,27 @@
+"""
+ASTERIX Cat0 Security Layer
+Basic Sender
+
+The basic sender plays the role of a legacy system that is not compatible with the security layer without a gateway.
+Its only role is to send messages over UDP multicast, as a basic ASTERIX radar should.
+"""
+
+import socket
+MULTICAST_IP = input("multicast ip?")
+MULTICAST_PORT = int(input("multicast port?"))
+
+sockmt = socket.socket(socket.AF_INET,
+                         socket.SOCK_DGRAM)
+sockmt.settimeout(0.5)
+DONE = False
+while not DONE:
+    message = ""
+    try:
+        message = input("> ")
+    except KeyboardInterrupt:
+        DONE = True
+        break
+    if message == "q":
+        DONE = True
+        break
+    sockmt.sendto(bytes(message, "ascii"), (MULTICAST_IP, MULTICAST_PORT))
