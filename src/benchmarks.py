032c098882cb9418297ecbb1c7b9e5fea86ae300
KirrimK <KirrimK.users.noreply@github.com>
2022-08-17 14:12:06 +0200

    added more benchmarks

--- src/benchmarks.py
+++ src/benchmarks.py
@@ -67,6 +67,67 @@ def benchmark_fernet():
     plt.plot(times_decipher, label="Deciphering")
     plt.savefig("fernet.png")
 
+def benchmark_eddsa_signatures():
+    print("Benchmarking EdDSA Signatures...")
+    signkey, verkey = lib.eddsa_generate()
+    times_sign = []
+    times_verify = []
+    for _ in range(0, 10000):
+        message = os.urandom(20) #replicate the secret that will be signed
+        start = time.perf_counter()
+        sign = lib.eddsa_sign(signkey, message)
+        _ = message + sign
+        end = time.perf_counter()
+        _ = lib.eddsa_verify(verkey, sign, message)
+        end_verify = time.perf_counter()
+        times_sign.append(end - start)
+        times_verify.append(end_verify - end)
+    print("Total time Signing: " + str(sum(times_sign)) + " seconds")
+    print("Average time for EdDSA signing: " + str(sum(times_sign) / len(times_sign)) + " seconds")
+    print("Max time for EdDSA signing: " + str(max(times_sign)) + " seconds")
+    print("Min time for EdDSA signing: " + str(min(times_sign)) + " seconds")
+    print("Total time Verifying: " + str(sum(times_verify)) + " seconds")
+    print("Average time for EdDSA verifying: " + str(sum(times_verify) / len(times_verify)) + " seconds")
+    print("Max time for EdDSA verifying: " + str(max(times_verify)) + " seconds")
+    print("Min time for EdDSA verifying: " + str(min(times_verify)) + " seconds")
+    plt.subplot(1, 2, 1)
+    plt.plot(times_sign, label="Signing")
+    plt.subplot(1, 2, 2)
+    plt.plot(times_verify, label="Verifying")
+    plt.savefig("eddsa_sign.png")
+    plt.clf()
+
+def benchmark_eddsa_encryption():
+    print("Benchmarking EdDSA Encryption...")
+    signkey, verkey = lib.eddsa_generate()
+    times_encrypt = []
+    times_decrypt = []
+    for _ in range(0, 10000):
+        message = os.urandom(20+64) #replicate the secret that will be encrypted after being signed
+        start = time.perf_counter()
+        ciphertext = lib.eddsa_encr(verkey, message)
+        end = time.perf_counter()
+        _ = lib.eddsa_decr(signkey, ciphertext)
+        end_decrypt = time.perf_counter()
+        times_encrypt.append(end - start)
+        times_decrypt.append(end_decrypt - end)
+    print("Total time Encrypting: " + str(sum(times_encrypt)) + " seconds")
+    print("Average time for EdDSA encryption: " + str(sum(times_encrypt) / len(times_encrypt)) + " seconds")
+    print("Max time for EdDSA encryption: " + str(max(times_encrypt)) + " seconds")
+    print("Min time for EdDSA encryption: " + str(min(times_encrypt)) + " seconds")
+    print("Total time Decrypting: " + str(sum(times_decrypt)) + " seconds")
+    print("Average time for EdDSA decryption: " + str(sum(times_decrypt) / len(times_decrypt)) + " seconds")
+    print("Max time for EdDSA decryption: " + str(max(times_decrypt)) + " seconds")
+    print("Min time for EdDSA decryption: " + str(min(times_decrypt)) + " seconds")
+    plt.subplot(1, 2, 1)
+    plt.plot(times_encrypt, label="Encrypting")
+    plt.subplot(1, 2, 2)
+    plt.plot(times_decrypt, label="Decrypting")
+    plt.savefig("eddsa_encr.png")
+    plt.clf()
+
 if __name__ == "__main__":
     benchmark_hmac()
     benchmark_fernet()
+    benchmark_eddsa_signatures()
+    benchmark_eddsa_encryption()
