07ca7cda2e0e8a7fcd4c0103ec106f8d583b2cf5
Andy <humbert.andy13@gmail.com>
2023-09-29 14:05:12 +0200

    added solo receiver and solo sender back 4 testing

---
+++ src/receiver_tesla_solo.py
@@ -0,0 +1,134 @@
+"""
+ASTERIX Cat0 Security Layer
+Receiver Gateway
+The Receiver Gateway processes all inbound ASTERIX traffic to verify if the messages have been signed by
+a legitimate sensor.
+This version uses georgesmakrakis implemention of TESLA protocol for authentification.
+(needs to add synchronisation protocol)
+"""
+import lib, json
+import sys
+import socket, struct
+import threading
+import logging
+from math import ceil, floor
+from time import time, sleep
+import TESLA.main_RFC as tesla
+import secrets
+logging.basicConfig(stream=sys.stdout, format="[%(asctime)s][%(levelname)s] - %(message)s", level=logging.INFO)
+logging.info("Started Receiver Gateway")
+# getting configuration from files
+CONFIG: dict = json.load(open(sys.argv[1], "r"))
+logging.info(f"Loaded configuration from \"{sys.argv[1]}\"")
+MULTICAST_IP: str = CONFIG["multicast_ip"]
+MULTICAST_PORT: int = CONFIG["multicast_port"]
+INTERFACE_IP: str = CONFIG["interface_ip"]
+logging.info(f"Listening for secure messages on IP addr {MULTICAST_IP}:{str(MULTICAST_PORT)}")
+logging.info(f"")
+logging.info("Configuration successfully loaded")
+###Socket send multicast
+mt_g = (MULTICAST_IP, MULTICAST_PORT)
+sockmts = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sockmts.settimeout(0.2)
+ttl = struct.pack('b',1)
+sockmts.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(INTERFACE_IP))
+###socket receive multicast
+server_address = (MULTICAST_IP, MULTICAST_PORT)
+sockmtr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+try:
+    sockmtr.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except AttributeError:
+    pass
+sockmtr.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sockmtr.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+sockmtr.bind(server_address)
+sockmtr.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(INTERFACE_IP))
+sockmtr.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
+                    socket.inet_aton(MULTICAST_IP)+ socket.inet_aton(INTERFACE_IP))
+MAX_KEY: str | None = None
+T_INT: float
+T0: float
+CHAIN_LENGHT: int
+DISCLOSURE_DELAY: int
+SENDER_TIME: float
+TIME_RESP: float
+NONCE = bytes(secrets.token_hex(16), 'utf-8')
+TIME_START : float | None = None
+TIME_END: float | None = None
+LIST_TIME= []
+TRIES = 0
+def listen():
+    global MAX_KEY, T_INT, T0, CHAIN_LENGHT, DISCLOSURE_DELAY, SENDER_TIME, TIME_RESP, NONCE, TIME_START, TIME_END, LIST_TIME, TRIES
+    try:
+        while True:
+                message, address = sockmtr.recvfrom(2048)
+                if message[:32] == NONCE: 
+                    logging.info(f"Received response to nonce from sender at {address}")
+                    TIME_RESP = time()
+                    MAX_KEY = str(message[32:64+32], 'utf-8') 
+                    other_values =struct.unpack('ddiid', message[64+32:])
+                    T_INT = float(other_values[0]) 
+                    T0 = float(other_values[1]) 
+                    CHAIN_LENGHT = int(other_values[2]) 
+                    DISCLOSURE_DELAY = int(other_values[3]) 
+                    SENDER_TIME = float(other_values[4]) 
+                    logging.info(f"Successfully receive parameters from sender at {address}")
+                elif message[:6] == b'Update':
+                    #logging.info(f"Updating key chain")
+                    nonce = message[6:38]
+                    updated_T = struct.unpack('dd', message[38+64:])
+                    tesla.update_receiver(last_key=str(message[38:38+64], encoding='utf-8'),T_int=float(updated_T[0]), T0=float(updated_T[1]), sender_interval=floor(((time()+receiver.D_t)-float(updated_T[1])) /  float(updated_T[0])), receiver=receiver)
+                    sockmts.sendto(b'Fup' + nonce, (MULTICAST_IP,MULTICAST_PORT))
+                elif message[:3] == b'fin':
+                    TIME_END = time()
+                    assert TIME_END != None and TIME_START != None
+                    tot_time = TIME_END-TIME_START
+                    LIST_TIME.append(tot_time)
+                    print(f"Average process time for one message at iteration {TRIES} is: {sum(LIST_TIME)/(TRIES*1000)}")
+                    print(f"Average process time for one message at iteration {TRIES} is: {sum(LIST_TIME)/(TRIES*10000)}")
+                    print(f"nb auth messages: {receiver.nb_authenticated_message}")
+                    #print(f"Total time: {tot_time}, average time: {tot_time/10000}")
+                elif message[:5] == b'start':
+                    TIME_START = time()
+                    TRIES += 1
+                elif len(message)>=100:
+                    recv_time = time()
+                    disclosed_key_index = message[-4:]
+                    disclosed_key = message[-68:-4]
+                    hmac = message[-100:-68]
+                    mes = message[:-100]
+                    packet = (mes, hmac, str(disclosed_key, encoding='utf-8'), int.from_bytes(disclosed_key_index, 'big', signed=True))
+                    #print(f"packet: {packet}")
+                    tesla.receive_message(packet=packet, receiver_obj=receiver, time = recv_time)
+    except Exception as e:
+        print(e)
+    logging.info("Socket timed out")
+def syncro_init():
+    receiver_time = time()
+    global NONCE, MULTICAST_IP, MULTICAST_PORT
+    sockmts.sendto(b'Nonce' + NONCE, (MULTICAST_IP,MULTICAST_PORT))
+    logging.info(f"Sent nonce {NONCE} to sender")
+    return receiver_time
+def syncro():
+    global MAX_KEY, T_INT, T0, CHAIN_LENGHT, DISCLOSURE_DELAY, SENDER_TIME, TIME_RESP, NONCE
+    receiver_time = syncro_init()
+    while MAX_KEY == None:
+        sleep(1)    
+    D_t = SENDER_TIME - receiver_time + 0.1
+    print(f"D_t: {D_t}")
+    sender_interval = floor((time()* 1000 - T0) * 1.0 / T_INT)
+    receiver = tesla.boostrap_receiver(last_key=MAX_KEY, T_int=T_INT, T0 = T0,
+                                        chain_length=CHAIN_LENGHT, disclosure_delay=DISCLOSURE_DELAY,
+                                          sender_interval=sender_interval, D_t=D_t)
+    return receiver
+if __name__=='__main__':
+    # start listening on socket sockmtr using a different thread
+    thd_syncro = threading.Thread(target=listen)
+    thd_syncro.start()
+    # On user input, syncronize the receiver with a sender 
+    print('press s to synchronize with sender')
+    while True:
+        key = input()
+        if key == 's':
+            receiver = syncro()
+     
---
+++ src/sender_tesla_solo.py
@@ -0,0 +1,123 @@
+"""
+ASTERIX Cat0 Security Layer
+Sender Gateway
+The Sender Gateway is the component of the system that adds a layer of authentication to all ASTERIX
+packets emitted by a sensor (such as a radar).
+It communicates with its clients to send signed messages and update the secret used for validation
+It uses georgesmakrakis implemention of TESLA protocol for authentification.
+(needs to add synchronisation protocol)
+"""
+import threading
+import lib, json
+import sys
+import socket, struct
+import time
+import logging
+import TESLA.main_RFC as tesla
+from math import ceil, floor
+from time import time, sleep # perf_counter, sleep
+import secrets
+logging.basicConfig(stream=sys.stdout, format="[%(asctime)s][%(levelname)s] - %(message)s", level=logging.INFO)
+logging.info("Started Sender gateway")
+# getting configuration from files
+config: dict = json.load(open(sys.argv[1], "r"))
+logging.info(f"Loaded configuration from \"{sys.argv[1]}\"")
+MULTICAST_IP: str = config["multicast_ip"]
+MULTICAST_PORT: int = config["multicast_port"]
+INTERFACE_IP: str = config["interface_ip"]
+logging.info(f"Listening for secure messages on IP addr {MULTICAST_IP}:{str(MULTICAST_PORT)}")
+logging.info(f"Interface ip is: {INTERFACE_IP}")
+logging.info("Configuration successfully loaded")
+###Socket send multicast
+mt_g = (MULTICAST_IP, MULTICAST_PORT)
+sockmts = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sockmts.settimeout(0.2)
+ttl = struct.pack('b',1)
+sockmts.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(INTERFACE_IP))
+###socket receive multicast
+server_address = (MULTICAST_IP, MULTICAST_PORT)
+sockmtr = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+try:
+    sockmtr.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+except AttributeError:
+    pass
+sockmtr.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sockmtr.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 1)
+sockmtr.bind(server_address)
+sockmtr.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(INTERFACE_IP))
+sockmtr.setsockopt(socket.SOL_IP, socket.IP_ADD_MEMBERSHIP,
+                    socket.inet_aton(MULTICAST_IP)+ socket.inet_aton(INTERFACE_IP))
+###Sender object
+private_seed = b"Hello world"
+N = 10
+rate_seconds = 0.05
+upper_bound_network_delay_seconds = 100
+rtt_seconds = 1
+sender = tesla.sender_setup(private_seed=private_seed, key_chain_length=N, rate_seconds=rate_seconds, upper_bound_network_delay_seconds=upper_bound_network_delay_seconds, rtt_seconds=rtt_seconds)
+max_key = sender.key_chain[len(sender.key_chain)-1]
+IS_UPDATING = False
+NONCE = None
+###Syncronisation
+def listen(max_key, T_int, T0, chain_lenght, disclosure_delay):
+    global NONCE, IS_UPDATING
+    try:
+        while True:
+            nonce, address = sockmtr.recvfrom(2048) 
+            if nonce[:5] == b'Nonce':
+                logging.info(f"Received nonce from receiver at {address}")
+                sender_time = time()
+                payload = nonce[5:] + bytes(max_key, 'utf-8') + struct.pack('ddiid', T_int, T0, chain_lenght, disclosure_delay, sender_time)
+                print(payload)
+                sockmts.sendto(payload, (MULTICAST_IP,MULTICAST_PORT))
+                logging.info(f"Sent sender time and necessary information to receiver at {address}")
+            if nonce[:3] == b'Fup':
+                if nonce[3:3+32] == NONCE:
+                    IS_UPDATING = False
+                    #logging.info(f"Finished updating key chain")
+            sleep(0.05/1000)
+    except Exception as e:
+        print(e)
+
+thd_syncro = threading.Thread(target=listen, args=(max_key, sender.T_int, sender.T0, N, sender.d))
+thd_syncro.start()
+
+
+def send_tesla_packet(message: bytes):
+    global IS_UPDATING, NONCE
+    message_time = time()
+    if message_time >= sender.last_T - sender.d * sender.T_int:
+        tesla.renew_key_chain(sender, message_time)
+        NONCE = bytes(secrets.token_hex(16), 'utf-8')
+        #print(f"{NONCE}, {bytes(sender.key_chain[0], 'utf-8')}, {sender.T_int, sender.T0}")
+        update_recv_packet = b"Update"+ NONCE + bytes(sender.key_chain[0], 'utf-8') + struct.pack("dd", sender.T_int, sender.T0)
+        IS_UPDATING = True
+        sockmts.sendto(update_recv_packet, (MULTICAST_IP,MULTICAST_PORT))
+        while IS_UPDATING == True:
+            sleep(0.01/1000)
+        packet = tesla.send_message(message=b"Disclosing previous key chain", sender_obj=sender, end=True)
+        packet_bytes = packet[0]+packet[1]+bytes(packet[2], 'utf-8')+ packet[3].to_bytes(4, byteorder='big',signed=True) # type: ignore
+        sockmts.sendto(packet_bytes, (MULTICAST_IP,MULTICAST_PORT))
+    tesla_packet = tesla.send_message(message=message, sender_obj=sender, end=False)
+    #print(tesla_packet)
+    #logging.info(f"Created tesla packet using the message {message}")
+    tesla_packet_bytes = tesla_packet[0]+tesla_packet[1]+bytes(tesla_packet[2], 'utf-8')+ tesla_packet[3].to_bytes(4, byteorder='big', signed=True) # type: ignore
+    sockmts.sendto(tesla_packet_bytes, (MULTICAST_IP,MULTICAST_PORT))
+    #print("sent")
+
+if __name__ == '__main__':
+
+    # start listening on the socket using a different thread
+    thd_syncro = threading.Thread(target=listen, args=(max_key, sender.T_int, sender.T0, N, sender.d))
+    thd_syncro.start()
+
+    # On user input, send 10 times 10000 messages in multicast.
+    print('press s to start sendin messages')
+    while True:
+        key = input()
+        if key == 's':
+            for a in range(10):
+                send_tesla_packet(message=b'start')
+                for i in range(10000):
+                    send_tesla_packet(message=f"{i}".encode("utf-8"))
+                    sleep(rate_seconds/1000)
+                send_tesla_packet(message=b'fin')
